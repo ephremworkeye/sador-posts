15757254ce8e72d1454675c96835ed6578bdd26b
Ephrem W <ephremworkeye@gmail.com>
2022-01-20 04:44:20 -0800

    added post list and single post vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Home</h1>
+  <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {title: 'hello', body:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita deleniti deserunt soluta, est ducimus sed voluptatibus porro ab qui architecto, maxime aspernatur ex nemo obcaecati voluptatem. Id praesentium, hic veritatis esse harum aspernatur dolorem eius ut aliquid. Quas dolorem voluptatibus iusto odio maiores. Obcaecati  ut? Explicabo nisi quibusdam corrupti, nobis consequuntur, tempore sed voluptates exercitationem expedita nam aut excepturi autem placeat vero, esse iure asperiores aspernatur facilis odit blanditiis fuga. Optio omnis consectetur quo nam dolor.', id: 1},
+      {title: 'second hello', body:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita deleniti deserunt soluta, est ducimus sed voluptatibus porro ab qui architecto, maxime aspernatur ex nemo obcaecati voluptatem. Id praesentium, hic veritatis esse harum aspernatur dolorem eius ut aliquid. Quas dolorem voluptatibus iusto odio maiores. Obcaecati  ut? Explicabo nisi quibusdam corrupti, nobis consequuntur, tempore sed voluptates exercitationem expedita nam aut excepturi autem placeat vero, esse iure asperiores aspernatur facilis odit blanditiis fuga. Optio omnis consectetur quo nam dolor', id: 2},
+    ])
+
+    return { posts }
   }
 }
 </script>
